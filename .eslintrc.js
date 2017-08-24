87f8d7444a82f0dbcc37faf8527ad96e68d7c426
ziphwy <ziphwy@gmail.com>
2017-08-24 10:41:02 +0800

    beautify the log, delete old common.js, add secureContext to avoid creating duplicate certificate

--- .eslintrc.js
+++ .eslintrc.js
@@ -7,10 +7,11 @@ module.exports = {
         "Config": false,
     },
     rules: {
-        'no-use-before-define': 0,
+        // 'no-use-before-define': 0,
         'no-underscore-dangle': 0,
-        'func-names': 0,
+        // 'func-names': 0,
         'no-plusplus': 0,
-        "no-param-reassign": 0
+        "no-param-reassign": 0,
+        'no-multi-assign':0
     }
 };
